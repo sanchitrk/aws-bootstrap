ddb0db8b092166057ba0112a52975fdf1640b567
Sanchit <sanchitrrk@gmail.com>
2022-05-05 10:23:56 +0530

    add codebuild and codedeploy spec

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -6,7 +6,21 @@ CLI_PROFILE=default
 
 EC2_INSTANCE_TYPE=t2.micro
 
-# Deploy the CloudFormation template
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --profile default --query "Account" --output text)
+
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+
+echo -e "\n\n=========== Deploying setup.yml ==========="
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME-setup \
+  --template-file setup.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+    CodePipelineBucket="$CODEPIPELINE_BUCKET"
+
 echo -e "\n\n=========== Deploying main.yml ==========="
 aws cloudformation deploy \
   --region $REGION \
